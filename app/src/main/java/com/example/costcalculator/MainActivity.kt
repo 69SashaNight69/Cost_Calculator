d041f9e4125f3706359831aa1f9e919611b7d218
SashaKolodko <sashanight69@gmail.com>
2025-09-20 20:32:43 +0300

    Module 1 Task 2

--- app/src/main/java/com/example/costcalculator/MainActivity.kt
+++ app/src/main/java/com/example/costcalculator/MainActivity.kt
@@ -12,16 +12,26 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.costcalculator.ui.theme.CostCalculatorTheme
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import android.util.Log
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        lifecycleScope.launch {
+            Log.d("ExpenseApp", "Початок фонового завдання...")
+            // Імітуємо затримку в 3 секунди
+            delay(3000L)
+            Log.d("ExpenseApp", "Фонове завдання завершено!")
+        }
         setContent {
             CostCalculatorTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Cost calculator",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -29,11 +39,30 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
+data class Expense(
+    val id: Long,          // Унікальний ідентифікатор витрати
+    val amount: Double,    // Сума витрати
+    val category: String,  // Категорія (наприклад, "Їжа", "Транспорт")
+    val description: String? // Опис (необов'язковий)
+)
 
+// Створюємо extension-функцію для класу Double
+fun Double.toCurrencyFormat(): String {
+    return "%.2f грн".format(this)
+}
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
+    // Створюємо об'єкт (екземпляр) нашого класу Expense
+    val sampleExpense = Expense(
+        id = 1L,
+        amount = 250.75,
+        category = "Продукти",
+        description = null // Приклад використання null
+    )
+
+    // Використовуємо нашу нову extension-функцію
     Text(
-        text = "Hello $name!",
+        text = "Категорія: ${sampleExpense.category}, Сума: ${sampleExpense.amount.toCurrencyFormat()}",
         modifier = modifier
     )
 }
